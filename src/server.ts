a2fd1ac896313d40e4d968000ee1ce91ba4d6fb4
obabakir <bubufighting01@gmail.com>
2026-05-24 22:13:38 +0900

    feat: integration of environmental variable

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
 // Architectural pattent: MVC, Dependency Injection,MVP
 
 // Design pattern: Middleware, Decorator
+
+// ==== Esma/Module Js ====
+// import moment from "moment";
+// ==== common js ====
+// // const moment = require('moment');
+// console.log("PORT:", process.env.PORT);
+// console.log("MONGO_URL:", process.env.MONGO_URL);
+
+console.log("Start");
+
+import dotenv from "dotenv";
+dotenv.config();
