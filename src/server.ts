25e9b4a6975c0364f6c8309d257d27f94a4383e7
obabakir <bubufighting01@gmail.com>
2026-05-24 22:58:18 +0900

    feat: connect mongodb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -13,3 +13,15 @@ console.log("Start");
 
 import dotenv from "dotenv";
 dotenv.config();
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("Succesfully connected:");
+    const PORT = process.env.PORT;
+  })
+  .catch((err) => {
+    console.log("ERROR on connection with mongodb:", err);
+  });
