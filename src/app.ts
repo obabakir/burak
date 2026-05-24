88c9a5104e4c3d1669fcd82ccbff4ab03f117c84
obabakir <bubufighting01@gmail.com>
2026-05-24 23:58:49 +0900

    feat: express integration

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+
+/** 1) ENTRENCE **/
+// ==== check the __dirname === later I will clean
+// console.log("__dirname:", __dirname);
+// console.log("__filename:", __filename);
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+/** 2) SESSION **/
+/** 3) VIEW **/
+app.set("views", path.join(__dirname, "views"));
+app.set("engine view", "ejs");
+/** 4) ROUTERS **/
+export default app; // module.exports = app;
--- src/server.ts
+++ src/server.ts
@@ -15,12 +15,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import mongoose from "mongoose";
-
+import app from "./app";
 mongoose
   .connect(process.env.MONGO_URL as string, {})
   .then((data) => {
-    console.log("Succesfully connected:");
+    console.log("Mongodb connected successfully:");
     const PORT = process.env.PORT;
+    app.listen(PORT, function () {
+      console.log(`The server is running successfully on port: ${PORT}`);
+    });
   })
   .catch((err) => {
     console.log("ERROR on connection with mongodb:", err);
