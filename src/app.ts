d840027c1283c66fa0663bc71742ebae5d361dde
obabakir <bubufighting01@gmail.com>
2026-06-07 16:38:32 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,14 @@ import routerAdmin from "./routerAdmin";
 import morgan from "morgan";
 import { MORGAN_CONFIG } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
 /** 1) ENTRENCE **/
 // ==== check the __dirname === later I will clean
 // console.log("__dirname:", __dirname);
@@ -16,7 +24,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_CONFIG));
 // ==== \n --> yangi qator tashlash mantigi
+
 /** 2) SESSION **/
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: { maxAge: 1000 * 3600 * 3 }, // 3 day
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 
 /** 3) VIEW **/
 app.set("views", path.join(__dirname, "views"));
