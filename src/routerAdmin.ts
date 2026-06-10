e2a6b68186330cfba61dead0a81a347d027edcd7
obabakir <bubufighting01@gmail.com>
2026-06-10 20:59:55 +0900

    feat: upload files into server via multer

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -2,6 +2,7 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant_controller ";
 import productController from "./controllers/product_controller";
+import makeUploader from "./libs/utils/uploader";
 
 // # ==========              ========= #
 // #             Restaurant             #
@@ -15,7 +16,11 @@ routerAdmin
 
 routerAdmin
   .get("/signup", restaurantController.getSignup)
-  .post("/signup", restaurantController.processSignup);
+  .post(
+    "/signup",
+    makeUploader("members").single("memberImage"),
+    restaurantController.processSignup,
+  );
 
 routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
@@ -32,6 +37,7 @@ routerAdmin.get(
 routerAdmin.post(
   "/product/create",
   restaurantController.verifyRestaurant,
+  makeUploader("products").array("productImages", 5),
   productController.createNewProduct,
 );
 
