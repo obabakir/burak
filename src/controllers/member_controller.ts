f44f3bfc9a62739859b8e47271fb7b8d6042ef71
obabakir <bubufighting01@gmail.com>
2026-07-03 14:26:27 +0900

    feat: create middleware methods and develop logout process

--- src/controllers/member_controller.ts
+++ src/controllers/member_controller.ts
@@ -1,8 +1,13 @@
 import { T } from "../libs/types/common";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 import MemberService from "../models/Member.service";
-import { Member, LoginInput, MemberInput } from "../libs/types/member";
+import {
+  Member,
+  LoginInput,
+  MemberInput,
+  ExtendedRequest,
+} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Error";
 import AuthService from "../models/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
@@ -60,21 +65,52 @@ memberController.login = async (req: Request, res: Response) => {
     else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
-
-memberController.verifyAuth = async (req: Request, res: Response) => {
+memberController.logout = async (req: ExtendedRequest, res: Response) => {
   try {
-    let member = null;
-    const token = req.cookies["accessToken"];
-    if (token) member = await authService.checkAuth(token);
+    console.log("logout");
+    res.cookie("accessToken", null, { maxAge: 0, httpOnly: true });
 
-    if (!member)
+    res.status(HttpCode.OK).json({ logout: true });
+  } catch (err) {
+    console.log("Error, logout", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.verifyAuth = async (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies["accessToken"];
+    if (token) req.member = await authService.checkAuth(token);
+
+    if (!req.member)
       throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
-    console.log("member:", member);
-    res.status(HttpCode.OK).json({ member: member });
+    // res.status(HttpCode.OK).json({ member: req.member });
+    // agar yuqoridagidan foydalansam response boradi postmanga bolmasam err korsataadi
+    next();
   } catch (err) {
     console.log("Error, verifyAuth", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+memberController.retrieveAuth = async (
+  req: ExtendedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies["accessToken"];
+    if (token) req.member = await authService.checkAuth(token);
+
+    next();
+  } catch (err) {
+    console.log("Error, retrieveAuth", err);
+    next();
   }
 };
 
--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -47,6 +47,11 @@ export interface MemberUpdateInput {
   memberDesc?: string;
   memberImages?: string;
 }
+export interface ExtendedRequest extends Request {
+  member: Member;
+  file: Express.Multer.File;
+  files: Express.Multer.File[];
+}
 
 export interface AdminRequest extends Request {
   member: Member;
